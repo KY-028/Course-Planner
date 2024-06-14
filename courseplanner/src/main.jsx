560f2252fa9b5291fc82063c14e8d2a40cec6b46
Kevin <yaokevin820@gmail.com>
2024-06-14 15:31:14 -0400

    Set up complete (?)

---
+++ courseplanner/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// DO NOT EVER MODIFY THIS FILE!!!
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
