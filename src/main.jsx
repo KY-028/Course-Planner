9fa975c737220f154659a57a0f49643c8bc447e3
Kevin <yaokevin820@gmail.com>
2024-06-14 16:08:03 -0400

    Tailwind should be up and running

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import "./styles.css";
 
 // DO NOT EVER MODIFY THIS FILE!!!
 ReactDOM.createRoot(document.getElementById('root')).render(
