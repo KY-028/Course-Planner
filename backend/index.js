b2d396006c5264c8ed6868b13136307726e6bd64
Karry Huang <karryhuang20030911@gmail.com>
2024-06-24 13:39:44 +0800

    backend structure update

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ import express from "express"
 import cors from "cors"
 import userRoutes from "./routes/users.js"
 import authRoutes from "./routes/auth.js"
+import courseChangeRoutes from "./routes/courseChange.js"
 import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(
     cors({
@@ -17,6 +18,7 @@ app.use(
 
 app.use("/backend/users", userRoutes);
 app.use("/backend/auth", authRoutes);
+app.use("/backend/courseChange", courseChangeRoutes);
 
 app.listen(8800, () => {
     console.log("connected!")
