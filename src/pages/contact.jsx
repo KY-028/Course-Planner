17bfd35c785d43b65e57151250f42c61392b852e
Kevin <yaokevin820@gmail.com>
2024-06-24 23:38:02 -0400

    Set Search Term back to nothing
    + CONTACT text white

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -26,8 +26,8 @@ const Contact = () => {
 
             <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', paddingTop: '20vh', paddingBottom: '40vh' }}>
                 <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-                    <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">Contact Us</h2>
+                    <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">See a technical issue or bug? Want to send feedback about a beta feature? Need details about our plan? Let us know!</p>
                     <form ref={form} onSubmit={handleSubmit} class="space-y-8">
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
